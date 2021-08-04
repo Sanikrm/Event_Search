24b62ef6741c56467ce384da02c7a13dc98d3199
Ara Oladipo <Oladipoeyiara@gmail.com>
2021-08-04 19:41:15 +0000

    Adding send to google maps button on location section

--- public/script.js
+++ public/script.js
@@ -18,12 +18,12 @@ let fullhtml = '';
 
 function viewMore(dates, name, locale, url, distance, images, info, classifications, eventlocation) {
     console.log(url)
-     document.querySelector("body").style.overflow = 'hidden'
+    document.querySelector("body").style.overflow = 'hidden'
     document.querySelector(".overlay2").style.display = 'flex'
     console.log(dates, name, locale, url, distance, images, info, classifications);
     document.querySelector('.viewmore--modal').innerHTML = `
       <div class="viewmore--img_hero">
-          <img src="${images[4].url}" class="viewmore--img">
+          <img src="${images[3].url}" class="viewmore--img">
           <div class="viewmote--hero_right">
               <h3 class="viewmodal--name">${name}</h3>
               <h5 class="viewmodal--extra">${dates.start.localDate} | ${classifications[0].segment.name}</h5>
@@ -32,41 +32,35 @@ function viewMore(dates, name, locale, url, distance, images, info, classificati
           </div>
         </div>
         <div class="locationsection">
+        <div>
         <h2 class="locationsectionh2">Location</h2>
+        <h3 class="locationtext">Check on google maps <a target="_blank" href="https://www.google.com/maps/@${eventlocation.latitude},${eventlocation.longitude},15z">here</a></h3>
+        </div>
                  <div id="mapid"></div>
 
       </div>
      `
 
-   var mymap = L.map('mapid').setView([eventlocation.latitude, eventlocation.longitude], 8);
+    var mymap = L.map('mapid').setView([eventlocation.latitude, eventlocation.longitude], 8);
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1
+    }).addTo(mymap);
 
-	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-		maxZoom: 18,
-		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
-			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-		id: 'mapbox/streets-v11',
-		tileSize: 512,
-		zoomOffset: -1
-	}).addTo(mymap);
+    L.marker([eventlocation.latitude, eventlocation.longitude]).addTo(mymap)
+        .bindPopup("<b>Event here!</b><br />").openPopup();
 
-	L.marker([eventlocation.latitude, eventlocation.longitude]).addTo(mymap)
-		.bindPopup("<b>Event here!</b><br />").openPopup();
-
-
-
-	var popup = L.popup();
-
-	function onMapClick(e) {
-		popup
-			.setLatLng(e.latlng)
-			.setContent("You clicked the map at " + e.latlng.toString())
-			.openOn(mymap);
-	}
-
-	mymap.on('click', onMapClick);
+        document.querySelector(".overlay2").addEventListener("click", function(){
+                document.querySelector("body").style.overflow = 'auto'
+    document.querySelector(".overlay2").style.display = 'none'
+        })
 }
 
-function renderCard({ dates, name, locale, url, distance, images, info, classifications, _embedded}) {
+function renderCard({ dates, name, locale, url, distance, images, info, classifications, _embedded }) {
     let html = `
           <div class="hero--event_2">
                 <img src="${images[2].url}" class="hero--img">
@@ -75,13 +69,13 @@ function renderCard({ dates, name, locale, url, distance, images, info, classifi
                 <h4 class="hero--eventdesc2">${info === undefined ? "There is no info on this event. If you want to read more about the event, click the I'm interested button below :D" : info}</h4>
                 <button class="learnmore" onclick='viewMore(${JSON.stringify(dates)},${JSON.stringify(name)}, ${JSON.stringify(locale)}, ${JSON.stringify(url)}, ${JSON.stringify(distance)}, ${JSON.stringify(images)}, ${JSON.stringify(info)}, ${JSON.stringify(classifications)}, ${JSON.stringify(_embedded.venues[0].location)})'>Learn More</button>
                <img src='images/icons8-heart-96.png' class="bookmark" onclick="pushToDB(${name}, ${dates}, ${locale}, ${url}, ${distance}, ${images}, ${info}, ${classifications})">
-
+ 
             </div>
   `
     fullhtml += html;
 }
 
-searchButton.addEventListener("click", function () {
+function searchEvent() {
     fullhtml = ''
     results.innerHTML = ''
     fetch('secret.json')
@@ -91,7 +85,7 @@ searchButton.addEventListener("click", function () {
             if (searchInput.value.length > 0) {
                 yourlocation = searchInput.value;
             }
-            const url = `https://app.ticketmaster.com/discovery/v2/events?apikey=${data["ticketmaster-api-key"]}&city=${yourlocation}&radius=50&unit=miles&locale=*&page=${currentPage}&sort=date,asc`;
+            const url = `https://app.ticketmaster.com/discovery/v2/events?apikey=${data["ticketmaster-api-key"]}&city=${yourlocation}&radius=50&unit=miles&locale=*&page=${currentPage}`;
             fetch(url)
                 .then(response => response.json())
                 .then(myjson => {
@@ -104,11 +98,13 @@ searchButton.addEventListener("click", function () {
                         results.insertAdjacentHTML("afterbegin", fullhtml);
                         showingresultsfor.textContent = "Showing results for " + searchInput.value
                     }
-                     results.scrollTo();
+                    results.scrollTo();
 
                 })
         })
-})
+}
+
+searchButton.addEventListener("click", searchEvent)
 
 function getLocation() {
     if (navigator.geolocation) {
@@ -139,7 +135,7 @@ function showPosition(position) {
                     for (events in data._embedded) {
                         data._embedded[events].forEach((eventdata) => renderCard(eventdata))
                         results.insertAdjacentHTML("afterbegin", fullhtml);
-                     }
+                    }
                 })
         })
         .catch(error => {
@@ -164,24 +160,22 @@ document.querySelector("#locateme").addEventListener("click", function () {
         console.log(`More or less ${crd.accuracy} meters.`);
 
         fetch(`https://geocode.xyz/${crd.latitude},${crd.longitude}?json=1`).then((res) => res.json()).then((res) => {
-            console.log(res)
-            yourlocation = res.city;
-
-            const words = yourlocation.split(" ");
-            for (const i in words) {
-                const word = words[i];
-                words[i] = word.slice(0, 1).toUpperCase() + word.toLowerCase().slice(1, word.length);
+            if (res.success !== false) {
+                yourlocation = res.city;
+                const words = yourlocation.split(" ");
+                for (const i in words) {
+                    const word = words[i];
+                    words[i] = word.slice(0, 1).toUpperCase() + word.toLowerCase().slice(1, word.length);
+                }
+                yourlocation = words.join(" ");
             }
-            yourlocation = words.join(" ");
+
+            console.log(yourlocation)
             searchInput.value = yourlocation;
 
             // searchInput.value = yourlocation.slice(0, 1).toUpperCase() + yourlocation.toLowerCase().slice(1, yourlocation.length);
         }).catch((err) => {
-            if (yourlocation != "") {
-                searchInput.value = yourlocation;
-            } else {
-                searchInput.value = "Try again!"
-            }
+            searchInput.value = yourlocation;
         });
 
     }
@@ -226,54 +220,54 @@ function pushToDB({ dates, name, locale, url, distance, images, info, classifica
 
 
 function signIn() {
-  var provider = new firebase.auth.GoogleAuthProvider();
-  firebase.auth()
-  .signInWithPopup(provider)
-  .then((result) => {
-    googleUser = result.user;    
-    closeAuthModal();
-    signupButton.innerText = "Log Out!";
-    signedIn = true;
-  }).catch(e => {
-    console.log(e);
-  });
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth()
+        .signInWithPopup(provider)
+        .then((result) => {
+            googleUser = result.user;
+            closeAuthModal();
+            signupButton.innerText = "Log Out!";
+            signedIn = true;
+        }).catch(e => {
+            console.log(e);
+        });
 }
 
 
 function emailSignIn(email, password) {
     firebase.auth().signInWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-        googleUser = userCredential.user;
-    })
-    .catch((error) => {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log(error)
-    });
+        .then((userCredential) => {
+            googleUser = userCredential.user;
+        })
+        .catch((error) => {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log(error)
+        });
 }
 
 function emailSignUp(email, password) {
     firebase.auth().createUserWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-        googleUser = userCredential.user;
-        console.log("Logged in!")
-    })
-    .catch((error) => {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        alert(errorMessage)
-    });  
+        .then((userCredential) => {
+            googleUser = userCredential.user;
+            console.log("Logged in!")
+        })
+        .catch((error) => {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            alert(errorMessage)
+        });
 }
 
 function signOut() {
     console.log("Signed out!")
     firebase.auth().signOut().then(() => {
-    signedIn = false;
-    signupButton.innerText = "Sign In!";
+        signedIn = false;
+        signupButton.innerText = "Sign In!";
     }).catch((error) => {
-    // An error happened.
-        
-    });   
+        // An error happened.
+
+    });
 }
 
 function openAuthModal() {
@@ -284,4 +278,10 @@ function openAuthModal() {
 function closeAuthModal() {
     document.querySelector("body").style.overflow = 'auto'
     document.querySelector(".overlay").style.display = 'none'
+}
+
+function searchSuggestion(city) {
+    yourlocation = city;
+    console.log(yourlocation)
+    searchEvent()
 }
